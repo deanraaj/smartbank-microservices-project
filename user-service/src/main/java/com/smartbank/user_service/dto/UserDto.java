74141189687de9e3c4d17ee22d1cd8827cbaa6b6
deanraaj <nagarajudola125@gmail.com>
2025-07-05 15:46:41 +0530

    Added sample feature to work with branches

--- user-service/src/main/java/com/smartbank/user_service/dto/UserDto.java
+++ user-service/src/main/java/com/smartbank/user_service/dto/UserDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 // Made change in the Central code base. NAGARAJU DOLA
+
+// WORKING WITH SAMPLE API
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
